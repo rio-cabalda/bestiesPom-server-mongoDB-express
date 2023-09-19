385979093c8d692ebe556d95dfe823ca8e990e82
rio-cabalda <riojoycabalda14@gmail.com>
2023-09-19 20:48:51 +0800

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -27,16 +27,37 @@ const server = http.createServer(app);
 
 const host = '192.168.1.32';
 const port = 8080;
-server.listen(port,host, ()=>{
-    console.log(`Server is running on http://${host}:${port}`);
-})
+
 
 
 // const MONGO_URL = 'mongodb+srv://rjcabalda:macie040921@cluster0.9wlmdtk.mongodb.net/?retryWrites=true&w=majority';
 
 mongoose.Promise = Promise;
-mongoose.connect(config.mongo.url,config.mongo.options);
+mongoose.connect(config.mongo.url,config.mongo.options)
+.then(() => {
+    console.log('Connected to MongoDB');
+    
+    // Middleware and route setup
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    
+    app.get('/', (req, res) => {
+      res.send('Hello, World!');
+    });
+
+    // Start the server after MongoDB connection is established
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
 mongoose.connection.on('error',(error: Error)=> console.log('mongo connection failed: ',error));
+
+server.listen(port,host, ()=>{
+    console.log(`Server is running on http://${host}:${port}`);
+})
 
 app.use('/',router());
 
