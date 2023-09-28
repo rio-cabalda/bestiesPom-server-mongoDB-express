7544cb624818cee0252960f4612aad16af3fac4d
rio-cabalda <riojoycabalda14@gmail.com>
2023-09-29 01:52:03 +0800

    Update checkInvalidatedToken.ts

--- src/middlewares/checkInvalidatedToken.ts
+++ src/middlewares/checkInvalidatedToken.ts
@@ -8,7 +8,7 @@ const checkInvalidatedToken = async(req:Request, res:Response, next:NextFunction
 
 
     if (!token) {
-        return res.status(401).json({ error: 'Unauthorized' });
+        return res.status(401).json({ error: 'Unauthorized access' });
       }
       
     try {
