c292d492c7bdbc771f3d0aa4821b5d56e80ace37
rio-cabalda <riojoycabalda14@gmail.com>
2023-09-29 01:52:06 +0800

    Update userCart.ts

--- src/router/userCart.ts
+++ src/router/userCart.ts
@@ -5,7 +5,7 @@ import validators from "../middlewares/validators/userValidation";
 
 export default (router: Router) => {
     router.get('/user/cart',userAuthentication, getUserCart); // get user's cart
-    router.post('/user/cart/:id',validators.quantity,userAuthentication, addToCart);
+    router.post('/user/cart/:id',validators.quantity, userAuthentication, addToCart);
     router.delete('/user/cart/clearcart',userAuthentication ,clearCart);
 
     router.post('/user/cart/update/:id',userAuthentication, updateCart);
