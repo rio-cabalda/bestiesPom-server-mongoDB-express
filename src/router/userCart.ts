2f690c03d44ec341bd0d660694b6b57798ca07d8
rio-cabalda <riojoycabalda14@gmail.com>
2023-10-08 09:39:19 +0800

    Update userCart.ts

--- src/router/userCart.ts
+++ src/router/userCart.ts
@@ -2,16 +2,14 @@ import {Router}  from "express";
 import { userAuthentication } from "../middlewares/AuthMiddleware";
 import { getUserCart, addToCart,updateCart, deleteCartItem, clearCart  } from '../controllers/cartController';
 import validators from "../middlewares/validators/userValidation";
+import checkInvalidatedToken from "../middlewares/checkInvalidatedToken";
 
 export default (router: Router) => {
-    router.get('/user/cart',userAuthentication, getUserCart); // get user's cart
-    router.post('/user/cart/:id',validators.quantity, userAuthentication, addToCart);
-    router.delete('/user/cart/clearcart',userAuthentication ,clearCart);
+    router.get('/user/cart',checkInvalidatedToken, userAuthentication, getUserCart); // get user's cart
+    router.post('/user/cart/:id',validators.quantity,checkInvalidatedToken, userAuthentication, addToCart);
+    router.delete('/user/cart/clearcart',checkInvalidatedToken, userAuthentication ,clearCart);
 
-    router.post('/user/cart/update/:id',userAuthentication, updateCart);
-
+    router.post('/user/cart/update/:id',checkInvalidatedToken, userAuthentication, updateCart);
 
     router.post('/user/cart/delete/:id', deleteCartItem);
 }
-
-
