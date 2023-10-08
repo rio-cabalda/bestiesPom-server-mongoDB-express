7ef08fc1e4893d8d75fef664743759bf6fb4b58a
rio-cabalda <riojoycabalda14@gmail.com>
2023-10-08 09:39:17 +0800

    Update AuthMiddleware.ts

--- src/middlewares/AuthMiddleware.ts
+++ src/middlewares/AuthMiddleware.ts
@@ -9,18 +9,18 @@ const jwtSecret = process.env.SERVER_TOKEN_SECRET;
 
 export const userAuthentication = async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
   const token = req.header('Authorization')?.replace('Bearer ', '');
-
+  
   if (!token) {
     return res.status(401).json({ error: 'Authorization token not provided' });
   }
   try {
-     const {user} = jwt.verify(token, jwtSecret) as { user: UserType};
-   
-     const verifiedUser = await getUserById(user._id)
+    const {user} = jwt.verify(token, jwtSecret) as { user: UserType};
+    const verifiedUser = await getUserById(user._id)
 
   if(!verifiedUser) return res.status(403).json({message: 'Invalid user'})
 
     req.user = verifiedUser; // Attach the user to the request object
+    
     next(); // Move to the next middleware or route handler
   } catch (error) {
     return res.status(401).json({ error: 'Invalid token' });
@@ -34,11 +34,9 @@ export const adminAuthentication = async (req: AuthenticatedRequest, res: Respon
     return res.status(401).json({ error: 'Authorization token not provided' });
   }
   try {
-     const { admin } = jwt.verify(token, jwtSecret) as { admin: UserType };
+    const { admin } = jwt.verify(token, jwtSecret) as { admin: UserType };
 
-     const verifiedAdmin = await getAdminById(admin._id);
-     
-    console.log(verifiedAdmin);
+    const verifiedAdmin = await getAdminById(admin._id);
     
     if(!verifiedAdmin) return res.status(403).json({message: 'Invalid administrator'})
 
