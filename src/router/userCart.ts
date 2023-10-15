27334dfce53fe5cb4f171ed76b379f85e051a1ba
rio-cabalda <riojoycabalda14@gmail.com>
2023-10-16 01:06:17 +0800

    Update userCart.ts

--- src/router/userCart.ts
+++ src/router/userCart.ts
@@ -3,6 +3,7 @@ import { userAuthentication } from "../middlewares/AuthMiddleware";
 import { getUserCart, addToCart,updateCart, clearCart  } from '../controllers/cartController';
 import validators from "../middlewares/validators/userValidation";
 import checkInvalidatedToken from "../middlewares/checkInvalidatedToken";
+const path = require('path');
 
 export default (router: Router) => {
     router.get('/user/cart',checkInvalidatedToken, userAuthentication, getUserCart); // get user's cart
@@ -10,4 +11,7 @@ export default (router: Router) => {
     router.delete('/user/cart/clearcart',checkInvalidatedToken, userAuthentication ,clearCart);
 
     router.post('/user/cart/update/:id',checkInvalidatedToken, userAuthentication, updateCart); // this route has update quantity and delete item by passing 0 value of quantity.
+    router.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'public/index.html'));
+      });
 }
